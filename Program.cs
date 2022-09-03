1d99d51b9b7070d3609e40b6754ea0538bec77d1
Rafael Mota <faelmota7@gmail.com>
2022-09-03 20:41:57 +0000

    Resolução

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine();
+Console.WriteLine("O professor:");
+Console.WriteLine("– Quem se acha burro fique em pé.");
+Console.WriteLine("Joãozinho se levanta:");
+Console.WriteLine("– Você se acha burro, Joãozinho?");
+Console.WriteLine("– Não, mas fiquei com dó de ver o senhor em pé sozinho.");
+Console.WriteLine();
